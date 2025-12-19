10f7116cf10ee88988b2d24414f7e7f431e922a7
Té Verde <hikih020@gmail.com>
2025-12-19 05:09:00 +0100

    reaccion

---
+++ back/controller/reaccionController.js
@@ -0,0 +1,7 @@
+const db = require('../config/database');
+
+
+
+module.exports = {
+
+};
--- back/server.js
+++ back/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const estado        = require('./routes/estadoRoute');
 const estudiante    = require('./routes/estudianteRoute');
+const reaccion      = require('./routes/reaccionRoute');
 const reporte       = require('./routes/reporteRoute');
 const tipoProblema  = require('./routes/tipo_problemaRoute');
 const ubicacion     = require('./routes/ubicacionRoute');
@@ -18,12 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use('/api/usuario', usuario);
-app.use('/api/estudiante', estudiante);
-app.use('/api/reporte', reporte);
 app.use('/api/estado', estado);
+app.use('/api/estudiante', estudiante);
+app.use('/api/reaccion', reaccion);
+app.use('/api/reporte', reporte);
 app.use('/api/tipoProblema', tipoProblema);
 app.use('/api/ubicacion', ubicacion);
+app.use('/api/usuario', usuario);
 
 app.get('/', (req, res) => {
     res.json({
